f31641f7722cd3c6f4a05a8580d0695cd2723115
Asger <asgerergod@hotmail.com>
2016-10-07 13:09:42 +0200

    Assignment 4

---
+++ Assignment4/src/assignment4/Answers.java
@@ -0,0 +1,33 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package assignment4;
+
+/**
+ *
+ * @author Asger
+ */
+public class Answers {   
+    /*
+    Question 5:
+    Case 1) "I am a PhD student"
+    Case 2) "I am a PhD student"
+    Case 3) "I am a TA"
+    Case 4) "Error" - cannot compile because student is not a TA
+    Case 5) "Error" - cannot compile because TA & PhD are not the same classes.
+    
+    Question 6:
+    student = phd;              // line 1 
+    student.display();          // line 2
+    - Console: "I am a PhD student"
+    
+    student = (student) phd;    // line 1
+    student.display();          // line 2
+    - Console: "I am a regular student"
+    
+    Question 8:
+    This will not compile because the super() constructor has parent class to call 
+    */ 
+}
